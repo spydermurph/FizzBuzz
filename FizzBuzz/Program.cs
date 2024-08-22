c56ec4b613dbd9b91e7db24c15c2a00cfb167107
spydermurph <69465629+spydermurph@users.noreply.github.com>
2024-08-22 10:14:30 +0000

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+//Environment.GetCommandLineArgs();
+int value = 100;
+if (args.Length > 0)
+{
+    value = int.Parse(args[0]);
+}
+
+Console.WriteLine($"Value: {value}");
+
+StringBuilder stringBuilder = new StringBuilder();
+for (int i = 1; i <= value; i++)
+{
+    if(i%3 == 0)
+    {
+        stringBuilder.Append("Fizz");
+    }
+    if (i%5 == 0)
+    {
+        stringBuilder.Append("Buzz");
+    }
+    Console.WriteLine(stringBuilder.Length == 0 ? i : stringBuilder.ToString());
+    stringBuilder.Clear();
+    
+}
